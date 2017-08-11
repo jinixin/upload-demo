63bb6aafde82f6757007ad92e8d89a31604f0cea
jinixin <jixinmanage@qq.com>
2017-08-11 23:34:39 +0800

    增加上传控件与显示页面
    Signed-off-by: jinixin <jixinmanage@qq.com>

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 # coding=utf-8
 
-from flask import Flask
+from flask import Flask, render_template as rt
+
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def index():
+    return rt('./index.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
