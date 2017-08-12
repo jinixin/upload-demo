53c757d4b245073e9114aed8bcac52e8e6519ed4
jinixin <jixinmanage@qq.com>
2017-08-12 23:20:14 +0800

    完成基本上传功能
    Signed-off-by: jinixin <jixinmanage@qq.com>

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 # coding=utf-8
 
-from flask import Flask, render_template as rt
+from flask import Flask, request, render_template as rt
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
+    return rt('./index.html')
+
+
+@app.route('/', methods=['POST'])
+def accept_file():
+    upload_file = request.files['file']
+    filename = secure_filename(upload_file.filename)
+    upload_file.save('upload/%s' % filename)
     return rt('./index.html')
 
 
