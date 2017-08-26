364492f7a0a4dae4f54f718b4e7ed82dfc4ec266
jinixin <jixinmanage@qq.com>
2017-08-27 01:31:32 +0800

    增加server注释
    Signed-off-by: jinixin <jixinmanage@qq.com>

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding=utf-8
 
 import os
@@ -7,36 +8,36 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def index():  # 一个分片上传后被调用
     if request.method == 'POST':
         upload_file = request.files['file']
-        task = request.form.get('task_id')
-        chunk = request.form.get('chunk', 0)
-        filename = '%s%s' % (task, chunk)
-        upload_file.save('./upload/%s' % filename)
+        task = request.form.get('task_id')  # 获取文件唯一标识符
+        chunk = request.form.get('chunk', 0)  # 获取该分片在所有分片中的序号
+        filename = '%s%s' % (task, chunk)  # 构成该分片唯一标识符
+        upload_file.save('./upload/%s' % filename)  # 保存分片到本地
     return rt('./index.html')
 
 
 @app.route('/success', methods=['GET'])
-def upload_success():
+def upload_success():  # 所有分片均上传完后被调用
     task = request.args.get('task_id')
     ext = request.args.get('ext', '')
     upload_type = request.args.get('type')
     if len(ext) == 0 and upload_type:
         ext = upload_type.split('/')[1]
-    ext = '' if len(ext) == 0 else '.%s' % ext
+    ext = '' if len(ext) == 0 else '.%s' % ext  # 构建文件后缀名
     chunk = 0
-    with open('./upload/%s%s' % (task, ext), 'w') as target_file:
+    with open('./upload/%s%s' % (task, ext), 'w') as target_file:  # 创建新文件
         while True:
             try:
                 filename = './upload/%s%d' % (task, chunk)
-                source_file = open(filename, 'r')
-                target_file.write(source_file.read())
+                source_file = open(filename, 'r')  # 按序打开每个分片
+                target_file.write(source_file.read())  # 读取分片内容写入新文件
                 source_file.close()
             except IOError:
                 break
             chunk += 1
-            os.remove(filename)
+            os.remove(filename)  # 删除该分片，节约空间
     return rt('./index.html')
 
 
